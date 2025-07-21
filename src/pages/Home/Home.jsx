921ca694624ccb2edab783777142de8589bdbfb2
TanveenAmbrose <racoctanveen15@gmail.com>
2025-07-21 15:39:15 +0600

    ok

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -23,6 +23,12 @@ const Home = () => {
           <TitleCards/>
         </div>
       </div>
+      <div className="more-cards">
+        <TitleCards/>
+        <TitleCards/>
+        <TitleCards/>
+        <TitleCards/>
+      </div>
     </div>
   )
 }
