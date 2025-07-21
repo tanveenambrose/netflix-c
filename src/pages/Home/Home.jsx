bbda037d99f57ec76c323e54690cb777fcafb549
TanveenAmbrose <racoctanveen15@gmail.com>
2025-07-21 16:03:56 +0600

    OK DONE

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -24,10 +24,10 @@ const Home = () => {
         </div>
       </div>
       <div className="more-cards">
-        <TitleCards/>
-        <TitleCards/>
-        <TitleCards/>
-        <TitleCards/>
+        <TitleCards title={"Blockbuster Movies"}/>
+        <TitleCards title={"Only on Netflix"}/>
+        <TitleCards title={"Upcoming"}/>
+        <TitleCards title={"Top Pics for You"}/>
       </div>
     </div>
   )
