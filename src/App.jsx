bfe87975d5a387117713631824909b63b2096921
TanveenAmbrose <racoctanveen15@gmail.com>
2025-07-22 14:33:23 +0600

    Ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import './index.css'
 import Home from './pages/Home/Home'
-import TitleCards from './components/TitleCards/TitleCards'
-import Footer from './components/Footer/Footer'
+
 
 const App = () => {
   return (
     <div>
       <Home/>
-      <Footer/>
+      
     </div>
   )
 }
